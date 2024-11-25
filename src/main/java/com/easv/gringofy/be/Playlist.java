7b8661f2311af8a3ef613308c1d5dfcbdc40ab42
Pawel <69588920+pawel-easv@users.noreply.github.com>
2024-11-25 18:44:54 +0100

    Changed business entities and background

--- src/main/java/com/easv/gringofy/be/Playlist.java
+++ src/main/java/com/easv/gringofy/be/Playlist.java
@@ -1,4 +1,18 @@
 package com.easv.gringofy.be;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Playlist {
+    private Map<Integer, Song> songs = new LinkedHashMap<>();
+    private String title;
+
+    public Playlist(String title) {
+        this.title = title;
+    }
+    public void addSong(int position, Song song) {
+        songs.put(position, song);
+    }
 }
--- src/main/java/com/easv/gringofy/be/Song.java
+++ src/main/java/com/easv/gringofy/be/Song.java
@@ -1,4 +1,15 @@
 package com.easv.gringofy.be;
 
 public class Song {
+    private String title;
+    private String artist;
+    private String genre;
+    private int duration;
+
+    public Song(String title, String artist, String genre, int duration) {
+        this.title = title;
+        this.artist = artist;
+        this.genre = genre;
+        this.duration = duration;
+    }
 }
