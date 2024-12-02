e01bacd10e02fd9fb0fb8ea01bdc6ea390b3513e
Pawel <69588920+pawel-easv@users.noreply.github.com>
2024-12-02 12:20:34 +0100

    Playlist view
    # Conflicts:
    #	src/main/resources/com/easv/gringofy/css/Main.css
    #	src/main/resources/com/easv/gringofy/views/playlist-view.fxml
    #	target/classes/com/easv/gringofy/views/playlist-view.fxml

---
+++ src/main/java/com/easv/gringofy/gui/controllers/PlaylistBuilder.java
@@ -0,0 +1,7 @@
+package com.easv.gringofy.gui.controllers;
+
+import com.easv.gringofy.be.Playlist;
+import com.easv.gringofy.gui.MusicPlayer;
+
+public class PlaylistBuilder extends MusicPlayer {
+}
