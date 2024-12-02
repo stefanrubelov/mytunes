76ef11afae43966815452a9f6440249005b76404
Pawel <69588920+pawel-easv@users.noreply.github.com>
2024-12-02 11:00:01 +0100

    Added categories and playlists view

---
+++ src/main/java/com/easv/gringofy/gui/controllers/PlaylistsPageController.java
@@ -0,0 +1,9 @@
+package com.easv.gringofy.gui.controllers;
+
+import com.easv.gringofy.gui.MusicPlayer;
+import javafx.event.ActionEvent;
+
+public class PlaylistsPageController extends MusicPlayer {
+    public void showPlaylistForm(ActionEvent actionEvent) {
+    }
+}
