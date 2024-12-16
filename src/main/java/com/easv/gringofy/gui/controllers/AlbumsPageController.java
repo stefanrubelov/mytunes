155b9c56d3f56ada1051c1c331f283b8cd0aeb0c
Pawel <69588920+pawel-easv@users.noreply.github.com>
2024-12-16 18:37:37 +0100

    Added albums view and controller class

---
+++ src/main/java/com/easv/gringofy/gui/controllers/AlbumsPageController.java
@@ -0,0 +1,9 @@
+package com.easv.gringofy.gui.controllers;
+
+import com.easv.gringofy.gui.MusicPlayer;
+import javafx.event.ActionEvent;
+
+public class AlbumsPageController extends MusicPlayer {
+    public void showAlbumForm(ActionEvent actionEvent) {
+    }
+}
